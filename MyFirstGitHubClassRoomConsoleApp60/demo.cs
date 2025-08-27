e5511dc0682588739c64412c29c7e2e274fb06b1
Patrick <pfriis@outlook.com>
2025-08-27 10:58:38 +0200

    Dette er en demo

---
+++ MyFirstGitHubClassRoomConsoleApp60/demo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyFirstGitHubClassRoomConsoleApp60
+{
+    public class demo
+    {
+        public string Test {  get; set; }
+        public int Antal { get; set; }
+        public bool ErDetSandt { get; set; }
+    }
+}
